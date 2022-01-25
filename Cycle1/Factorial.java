8688890c84d5ae51ed39d3560d7f1844819d7eb4
abduabi2002 <98332447+abduabi2002@users.noreply.github.com>
2022-01-25 12:47:56 +0530

    Create Factorial.java

---
+++ Cycle1/Factorial.java
@@ -0,0 +1,17 @@
+//factorial of a given number
+import java.util.*;
+class f
+{
+    public static void main(String args[])
+    {
+        Scanner obj=new Scanner(System.in);
+        System.out.println("Enter  number:");
+        int x=obj.nextInt();
+        int i,fact=1;
+        for(i=1;i<=x;i++)
+        {
+            fact=fact*i;
+        }
+        System.out.println("Factorial is:"+fact);
+    }
+}
