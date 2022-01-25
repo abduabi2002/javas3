50009ba4aad91b81a65ef40e5c5c6b32ec666196
abduabi2002 <98332447+abduabi2002@users.noreply.github.com>
2022-01-25 12:45:46 +0530

    Create Fibanocci.java

---
+++ Cycle1/Fibanocci.java
@@ -0,0 +1,18 @@
+//fibanocci sequence
+import java.util.*;
+class c {
+    public static void main(String args[])
+    {
+        Scanner obj=new Scanner(System.in);
+        int i,a=0,b=1,c;
+        System.out.println("Enter  number:");
+        int x=obj.nextInt();
+        for(i=0;i<x;i++)
+        {
+            c=a+b;
+            System.out.println(c);
+            a=b;
+            b=c;
+        }
+    }
+}
