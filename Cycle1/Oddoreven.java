8fc04de03802bca3583223e6a776f2fdcb8abaf8
abduabi2002 <98332447+abduabi2002@users.noreply.github.com>
2022-01-25 12:46:29 +0530

    Create Oddoreven.java

---
+++ Cycle1/Oddoreven.java
@@ -0,0 +1,16 @@
+//check wheather given number is odd or even
+import java.util.*;
+class d {
+    public static void main(String args[])
+    {
+        Scanner obj=new Scanner(System.in);
+        int i;
+        System.out.println("Enter  number:");
+        int x=obj.nextInt();
+        if(x%2==0)
+            System.out.println("it is even");
+        else
+            System.out.println("it is odd");
+        
+    }
+}
