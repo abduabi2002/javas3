72d94bc504c73160d6da920769fdfc0b19e1ef80
abduabi2002 <98332447+abduabi2002@users.noreply.github.com>
2022-01-25 12:44:09 +0530

    Create Sum.java

---
+++ Cycle1/Sum.java
@@ -0,0 +1,16 @@
+//find the sum of two numbers
+import java.util.*;
+class a
+{
+    public static void main(String args[])
+    {
+        Scanner obj=new Scanner(System.in);
+        System.out.println("Enter first number:");
+        int n=obj.nextInt();
+        System.out.println("Enter second number:");
+        int m=obj.nextInt();
+        int sum=0;
+        sum=n+m;
+        System.out.println("Sum="+sum);
+    }
+}
